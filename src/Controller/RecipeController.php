e8a1271528223ea21174dea97d7c2b63fd85ad6f
Azixy Dev <azizchegrani97@gmail.com>
2024-07-05 10:51:21 +0200

    Initialisation du projet

---
+++ src/Controller/RecipeController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class RecipeController extends AbstractController
+{
+
+    #[Route('/recipes', name: 'recipe.index')]
+    public function index(Request $request): Response
+    {
+        return $this->render('recipe/index.html.twig');
+    }
+
+    #[Route('/recipe/{slug}-{id}', name: 'recipe.show', requirements: ['id' => '\d+', 'slug' => '[a-z0-9-]+'])]
+    public function show(Request $request, string $slug, int $id): Response
+    {
+        return $this->render('recipe/show.html.twig', [
+            'controller_name' => 'RecipeController',
+        ]);
+    }
+}
