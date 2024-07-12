ee4cb177fe17c281ae7edaca31646680a1e30959
Azixy Dev <azizchegrani97@gmail.com>
2024-07-12 21:45:57 +0200

    Ajout de l'administration et des catégories

--- src/Controller/Admin/RecipeController.php
+++ src/Controller/Admin/RecipeController.php
@@ -10,52 +10,20 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Requirement\Requirement;
 
+#[Route('/admin/recipes', name: 'admin.recipe.')]
 class RecipeController extends AbstractController
 {
 
-    #[Route('/recipes', name: 'recipe.index')]
+    #[Route('/', name: 'index')]
     public function index(Request $request, RecipeRepository $recipeRepository): Response
     {
         $recipes = $recipeRepository->findWithDurationLowerThan(40);
-        return $this->render('recipe/index.html.twig', ['recipes' => $recipes]);
+        return $this->render('admin/recipe/index.html.twig', ['recipes' => $recipes]);
     }
 
-    #[Route('/recipes/{slug}-{id}', name: 'recipe.show', requirements: ['id' => '\d+', 'slug' => '[a-z0-9-]+'])]
-    public function show(Request $request, string $slug, int $id, RecipeRepository $recipeRepository): Response
-    {
-
-        $recipe = $recipeRepository->find($id);
-
-        if ($recipe->getSlug() !== $slug) {
-            return $this->redirectToRoute('recipe.show', ['slug' => $recipe->getSlug(), 'id' => $recipe->getId()]);
-        }
-
-        return $this->render('recipe/show.html.twig', [
-            'recipe' => $recipe
-        ]);
-    }
-
-    #[Route('/recipes/{id}/edit', name: 'recipe.edit', methods: ['GET', 'POST'])]
-    public function edit(Recipe $recipe, Request $request, EntityManagerInterface $em): Response
-    {
-        $form = $this->createForm(RecipeType::class, $recipe);
-
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-
-            $em->flush();
-
-            $this->addFlash('success', 'La recette a bien été modifiée');
-
-            return $this->redirectToRoute('recipe.index');
-        }
-
-        return $this->render('recipe/edit.html.twig', ['recipe' => $recipe, 'form' => $form]);
-    }
-
-    #[Route('/recipes/create', name: 'recipe.create')]
+    #[Route('/create', name: 'create')]
     public function create(Request $request, EntityManagerInterface $em): Response
     {
         $recipe = new Recipe();
@@ -68,17 +36,36 @@ class RecipeController extends AbstractController
             $em->persist($recipe);
             $em->flush();
             $this->addFlash('success', 'La recette a bien été créée');
-            return $this->redirectToRoute('recipe.index');
+            return $this->redirectToRoute('admin.recipe.index');
         }
-        return $this->render('recipe/create.html.twig', ['form' => $form]);
+        return $this->render('admin/recipe/create.html.twig', ['form' => $form]);
     }
 
-    #[Route('/recipes/{id}', name: 'recipe.delete', methods: ['DELETE'])]
+    #[Route('/{id}', name: 'edit', methods: ['GET', 'POST'], requirements: ['id' => Requirement::DIGITS])]
+    public function edit(Recipe $recipe, Request $request, EntityManagerInterface $em): Response
+    {
+        $form = $this->createForm(RecipeType::class, $recipe);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $em->flush();
+
+            $this->addFlash('success', 'La recette a bien été modifiée');
+
+            return $this->redirectToRoute('admin.recipe.index');
+        }
+
+        return $this->render('admin/recipe/edit.html.twig', ['recipe' => $recipe, 'form' => $form]);
+    }
+
+    #[Route('/{id}', name: 'delete', methods: ['DELETE'], requirements: ['id' => Requirement::DIGITS])]
     public function remove(Recipe $recipe, EntityManagerInterface $em)
     {
         $em->remove($recipe);
         $em->flush();
         $this->addFlash('success', 'La recette a bien été supprimée');
-        return $this->redirectToRoute('recipe.index');
+        return $this->redirectToRoute('admin.recipe.index');
     }
 }
--- src/Form/CategoryType.php
+++ src/Form/CategoryType.php
@@ -2,19 +2,15 @@
 
 namespace App\Form;
 
-use App\Entity\Recipe;
+use App\Entity\Category;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Event\PostSubmitEvent;
-use Symfony\Component\Form\Event\PreSubmitEvent;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\String\Slugger\AsciiSlugger;
 
-class RecipeType extends AbstractType
+class CategoryType extends AbstractType
 {
     public function __construct(private FormListenerFactory $formListenerFactory)
     {
@@ -24,25 +20,26 @@ class RecipeType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title', TextType::class, [
+            ->add('name', TextType::class, [
                 'empty_data' => ''
             ])
-            ->add('slug')
-            ->add('content', TextareaType::class, [
+            ->add('slug', TextType::class, [
+                'required' => false,
                 'empty_data' => ''
             ])
-            ->add('duration')
             ->add('save', SubmitType::class, [
-                'label' => 'Envoyer'
+                'label' => 'Enregistrer'
             ])
-            ->addEventListener(FormEvents::PRE_SUBMIT, $this->formListenerFactory->autoSlug('title'))
+
+            ->addEventListener(FormEvents::PRE_SUBMIT, $this->formListenerFactory->autoSlug('name'))
             ->addEventListener(FormEvents::POST_SUBMIT, $this->formListenerFactory->timestamps());
+        ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => Recipe::class,
+            'data_class' => Category::class,
         ]);
     }
 }
