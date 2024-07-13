7b233f4850bc476e86feef851865c34e9de310f7
Azixy Dev <azizchegrani97@gmail.com>
2024-07-13 13:37:22 +0200

    Ajout des images aux recettes

--- src/Controller/Admin/RecipeController.php
+++ src/Controller/Admin/RecipeController.php
@@ -7,6 +7,7 @@ use App\Form\RecipeType;
 use App\Repository\RecipeRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
--- src/Form/RecipeType.php
+++ src/Form/RecipeType.php
@@ -6,12 +6,14 @@ use App\Entity\Category;
 use App\Entity\Recipe;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Image;
 
 class RecipeType extends AbstractType
 {
@@ -31,6 +33,7 @@ class RecipeType extends AbstractType
                 'class' => Category::class,
                 'choice_label' => 'name'
             ])
+            ->add('thumbnailFile', FileType::class)
             ->add('content', TextareaType::class, [
                 'empty_data' => ''
             ])
